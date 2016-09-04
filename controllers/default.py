ca81bae29fb94e28428e5b7d4b1cf04c2ac584a3
Daniel Hipke <daniel.hipke@cirrus10.com>
2016-09-03 21:15:12 -0700

    Got twitter sentiment analysis working. Made classifier cache in memory

--- controllers/default.py
+++ controllers/default.py
@@ -8,9 +8,71 @@
 # - download is for downloading files uploaded in the db (does streaming)
 # -------------------------------------------------------------------------
 import nltk.classify.util
-import classifier
 import requests
 import json
+import csv
+import os
+import cPickle as pickle
+from nltk import NaiveBayesClassifier, word_tokenize
+from nltk.corpus import movie_reviews
+
+
+def word_feats(words):
+    my_word_feats = dict([(word, True) for word in words])
+    #print my_word_feats
+    return my_word_feats
+
+def get_classifier():
+    classifier = cache.ram('classifier', lambda: get_classifier_from_disk(), time_expire=60*60*24)
+    return classifier
+
+def get_classifier_from_disk():
+    if os.path.isfile('my_classifier.pickle'):
+        classifier = pickle.load(open('my_classifier.pickle', 'rb'))
+    else:
+        #negids = movie_reviews.fileids('neg')
+        #posids = movie_reviews.fileids('pos')
+
+        negfeats = []
+        posfeats = []
+        #negfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'neg') for f in negids]
+        #posfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'pos') for f in posids]
+
+
+        with open(os.path.join(request.folder, 'data', 'twitter-sentiment-analysis-dataset.csv')) as csvfile:
+            tweet_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+            #skip first line
+            tweet_reader.next()
+            count = 0;
+            for tweet in tweet_reader:
+                words = word_tokenize(unicode(tweet[3].lower(), 'utf-8'))
+                if tweet[1] == '1':
+                    #print "positive tweet"
+                    posfeats.append((word_feats(words), 'pos'))
+                else:
+                    #print "negative tweet"
+                    negfeats.append((word_feats(words), 'neg'))
+                count += 1
+                print count
+                #if (count == 10):
+                #    break;
+
+        negcutoff = len(negfeats) * 9 / 10
+        poscutoff = len(posfeats) * 9 / 10
+
+        trainfeats = negfeats[:negcutoff] + posfeats[:poscutoff]
+        testfeats = negfeats[negcutoff:] + posfeats[poscutoff:]
+        print 'train on %d instances, test on %d instances' % (len(trainfeats), len(testfeats))
+
+        classifier = NaiveBayesClassifier.train(trainfeats)
+        pickle.dump(classifier, open('my_classifier.pickle', 'wb'))
+        pickle.dump(testfeats, open('testfeats.pickle', 'wb'))
+        print 'accuracy:', nltk.classify.util.accuracy(classifier, testfeats)
+        classifier.show_most_informative_features()
+    return classifier
+
+def get_testfeats():
+    return pickle.load(open('testfeats.pickle', 'rb'))
 
 def extract_features(document):
     document_words = set(document)
@@ -21,7 +83,7 @@ def extract_features(document):
     return features
 
 def index():
-    my_classifier = classifier.get_classifier()
+    my_classifier = get_classifier()
     video_id = request.vars['videoId']
     url = 'https://www.googleapis.com/youtube/v3/commentThreads?part=id,snippet&videoId=' + video_id + '&order=relevance&key=AIzaSyB1NMklzwIEwson5THrPWB7eyfJ3o30Ydg'
     response = requests.get(url)
